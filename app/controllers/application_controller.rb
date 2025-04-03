2fafc4e98849bae14a52957651da1edf151e5308
murray-pocha <murray.pocha@gmail.com>
2025-04-03 10:26:41 -0600

    implement user auth with register and logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,4 +29,10 @@ class ApplicationController < ActionController::Base
     }
     cookies[:cart]
   end
+
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
@@ -11,6 +12,10 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
+
+  get '/register', to: 'users#new', as: 'register'
+  post '/users', to: 'users#create'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
 
   namespace :admin do
     root to: 'dashboard#show'
